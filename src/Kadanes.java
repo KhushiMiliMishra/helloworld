3bdda4fde384144c93655cb97f06d9e62d8ef2e1
KhushiMiliMishra <khusmish1020@gmail.com>
2026-06-28 11:11:51 +0530

    kadanes algorithm

---
+++ src/Kadanes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Kadanes {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int currSum=arr[0];
+        int bestSum=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>(arr[i]+currSum)) currSum=arr[i];
+            else currSum+=arr[i];
+            bestSum=Math.max(bestSum,currSum);
+        }
+        System.out.println(bestSum);
+    }
+}
